7e3ecdc1356649416da2d0248803189ac1664b43
Jennifer Gauthier <jennifergauthier_1@hotmail.com>
2023-05-02 15:39:14 -0400

    changement api

--- js/app.js
+++ js/app.js
@@ -1,12 +1,8 @@
-const fetch = require('node-fetch');
-require('dotenv').config();
-
 const url = 'https://api.github.com/users/JenGaut';
 const options = {
   method: 'GET',
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
   },
 };
 
@@ -17,7 +13,9 @@ fetch(url, options)
     const profileUrl = data.html_url;
   
     // Mettre à jour les éléments HTML avec les données de l'API GitHub
-    document.getElementById('github-username').innerHTML = `<a href="${profileUrl}">${username}</a>`;
+    document.getElementById('github-username').innerHTML = username;
+    document.getElementById('github-username').setAttribute('href', profileUrl);
     document.getElementById('github-repos').textContent = data.public_repos;
   })
   .catch(error => console.error(error));
+
