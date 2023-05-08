768926a92187fae9fbc902e39e52d136234c90c7
Jennifer Gauthier <jennifergauthier_1@hotmail.com>
2023-05-08 12:10:55 -0400

    api ?

--- js/app.js
+++ js/app.js
@@ -1,20 +1,18 @@
-window.onload = function() {
-  const url = 'https://api.github.com/users/JenGaut';
-  const options = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+const url = "https://api.github.com/users/JenGaut";
+const options = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
-  fetch(url, options)
-    .then(response => response.json())
-    .then(data => {
-      const username = data.login;
-      const profileUrl = data.html_url;
+fetch(url, options)
+  .then((response) => response.json())
+  .then((data) => {
+    const username = data.login;
+    const profileUrl = data.html_url;
 
-      document.getElementById('github-username').innerHTML = username;
-      document.getElementById('github-username').setAttribute('href', profileUrl);
-    })
-    .catch(error => console.error(error));
-}
+    document.getElementById("github-username").innerHTML = username;
+    document.getElementById("github-username").setAttribute("href", profileUrl);
+  })
+  .catch((error) => console.error(error));
